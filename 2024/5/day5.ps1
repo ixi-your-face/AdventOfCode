bb71b43376b78364dc77e4f8bd1d4d9d192660ed
= <17403850+ixi-your-face@users.noreply.github.com>
2024-12-05 23:22:09 +0000

    how pretty

--- 2024/5/day5.ps1
+++ 2024/5/day5.ps1
@@ -5,34 +5,6 @@ param (
     $InputFile = ".\input.txt"
 )
 
-$PageInstructions = (Get-Content $InputFile)
-$PageRules = [ArrayList]::New()
-$PageOrders = [ArrayList]::New()
-$ValidPages = [ArrayList]::New()
-$InvalidPages = [ArrayList]::New()
-$Part1Totals = 0
-$Part2Totals = 0
-
-
-foreach($Line in $PageInstructions){
-    switch ($Line) {
-        {$_.Contains('|')} {
-            $SplitLine = $Line.Split('|')
-
-            $Rule = [PSCustomObject]@{
-                Before = $SplitLine[0]
-                After = $SplitLine[1]
-            }
-
-            [Void]$PageRules.Add($Rule)
-        }
-        {$_.Contains(',')} {[Void]$PageOrders.Add([ArrayList]$Line.Split(','))}
-        Default {continue}
-    }
-}
-
-$OrderedRules = ($PageRules | Sort-Object Before | Group Before -AsHashTable)
-
 function Test-Valid ($OrderedRules, $Page){
     foreach($Number in $Page){
         $NumberRules = $OrderedRules.$Number.After
@@ -52,10 +24,64 @@ function Test-Valid ($OrderedRules, $Page){
     return $true
 }
 
-foreach($Update in $PageOrders){
-    $IsValid = (Test-Valid $OrderedRules $Update)
+function Resolve-RuleViolations($OrderedRules, $InvalidPage){
+    for($i = 0; $i -lt $InvalidPage.Count; $i++){
+        $Number = $InvalidPage[$i]
+        $NumberRules = $OrderedRules.$Number.After
+        $NumberIndex = $InvalidPage.IndexOf($Number)
 
-    if($IsValid){
+        foreach($Rule in $NumberRules){
+            $RuleIndex = $InvalidPage.IndexOf($Rule)
+
+            if($RuleIndex -eq -1){ continue }
+
+            if($RuleIndex -lt $NumberIndex){
+                [Void]$InvalidPage.Remove($Number)
+                [Void]$InvalidPage.Insert($RuleIndex, $Number)
+            }
+        }
+    }
+}
+
+function Get-MiddleTotal($Pages){
+    $Output = 0
+
+    foreach($Page in $Pages){
+        $MiddleNumber = [int]$Page[[Math]::Floor($Page.Count/2)]
+    
+        $Output += $MiddleNumber
+    }
+
+    return $Output
+}
+
+$PageInstructions = (Get-Content $InputFile)
+$PageRules = [ArrayList]::New()
+$PageOrders = [ArrayList]::New()
+$ValidPages = [ArrayList]::New()
+$InvalidPages = [ArrayList]::New()
+
+foreach($Line in $PageInstructions){
+    switch ($Line) {
+        {$_.Contains('|')} {
+            $SplitLine = $Line.Split('|')
+
+            $Rule = [PSCustomObject]@{
+                Before = $SplitLine[0]
+                After = $SplitLine[1]
+            }
+
+            [Void]$PageRules.Add($Rule)
+        }
+        {$_.Contains(',')} {[Void]$PageOrders.Add([ArrayList]$Line.Split(','))}
+        Default {continue}
+    }
+}
+
+$OrderedRules = ($PageRules | Sort-Object Before | Group-Object Before -AsHashTable)
+
+foreach($Update in $PageOrders){
+    if(Test-Valid $OrderedRules $Update){
         [Void]$ValidPages.Add($Update)
     } else {
         [Void]$InvalidPages.Add($Update)
@@ -64,36 +90,9 @@ foreach($Update in $PageOrders){
 
 foreach($InvalidPage in $InvalidPages){
     while(!(Test-Valid $OrderedRules $InvalidPage)){
-        for($i = 0; $i -lt $InvalidPage.Count; $i++){
-            $Number = $InvalidPage[$i]
-            $NumberRules = $OrderedRules.$Number.After
-            $NumberIndex = $InvalidPage.IndexOf($Number)
-    
-            foreach($Rule in $NumberRules){
-                $RuleIndex = $InvalidPage.IndexOf($Rule)
-    
-                if($RuleIndex -eq -1){ continue }
-    
-                if($RuleIndex -lt $NumberIndex){
-                    [Void]$InvalidPage.Remove($Number)
-                    [Void]$InvalidPage.Insert($RuleIndex, $Number)
-                }
-            }
-        }
+        Resolve-RuleViolations $OrderedRules $InvalidPage
     }
 }
 
-foreach($ValidPage in $ValidPages){
-    $MiddleNumber = [int]$ValidPage[[Math]::Floor($ValidPage.Count/2)]
-
-    $Part1Totals += $MiddleNumber
-}
-
-foreach($InvalidPage in $InvalidPages){
-    $MiddleNumber = [int]$InvalidPage[[Math]::Floor($InvalidPage.Count/2)]
-
-    $Part2totals += $MiddleNumber
-}
-
-Write-Host "Part 1: $Part1Totals"
-Write-Host "Part 2: $Part2Totals"
+Write-Host "Part 1: $(Get-MiddleTotal $ValidPages)"
+Write-Host "Part 2: $(Get-MiddleTotal $InvalidPages)"
